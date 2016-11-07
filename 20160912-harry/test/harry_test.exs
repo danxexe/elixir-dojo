23d961b5a3593b0cebb22062615e864569b2dfa3
DanX <danx.exe@gmail.com>
2016-11-07 14:23:26 -0200

    Solution not done during Dojo

--- 20160912-harry/test/harry_test.exs
+++ 20160912-harry/test/harry_test.exs
@@ -1,36 +1,38 @@
 defmodule Harry do
+  def price(items) when is_tuple(items) do
+    Tuple.to_list(items)
+    |> price
+  end
+
   def price(items) do
-    price(0,items)
+    price(0, items)
   end
 
-  def price(acc, items={n, j, k, l, m}) do
-    distinct_books = Tuple.to_list(items)
-    |> Enum.filter(fn (e) -> e > 0 end)
-    |> Enum.count
-
-    # distinct_books = Enum.reduce Tuple.to_list(items), 0 , positive
-
-    if distinct_books == 0 do
-      acc
-    else
-      acc + price(n * 8 * (1 - discounts(distinct_books)), {n-1, j-1, k-1, l-1, m-1})
-    end 
+  def price(amount, [0, 0, 0, 0, 0]) do
+    amount
   end
 
-  def count_positive(item, acc) do
-    if item > 0, do: acc + 1, else: acc
+  def price(amount, items) do
+    {distinct_books, new_items} = count_positive_and_decrement(items)
+    new_amount = amount + distinct_books * 8 * discount(distinct_books)
+    price(new_amount, new_items)
   end
 
-  def discounts(n) do
-    %{
-      1 => 0,
-      2 => 0.05,
-      3 => 0.10,
-      4 => 0.15,
-      5 => 0.20
-    }[n]
-  end
+  [0, 0.05, 0.10, 0.15, 0.20]
+  |> Enum.with_index(1)
+  |> Enum.map(fn ({val, n}) ->
+    def discount(unquote(n)) do
+      1 - unquote(val)
+    end
+  end)
 
+  def count_positive_and_decrement(items) do
+    items
+    |> Enum.reduce({0, []}, fn (item, {positive, new_items}) ->
+      {positive, new_item} = if item > 0, do: {positive + 1, item - 1}, else: {positive, item}
+      {positive, new_items ++ [new_item]}
+    end)
+  end
 end
 
 defmodule HarryTest do
@@ -38,7 +40,7 @@ defmodule HarryTest do
   doctest Harry
 
   test "1 book" do
-    assert Harry.price(0, {1, 0, 0, 0, 0}) == 8
+    assert Harry.price({1, 0, 0, 0, 0}) == 8
   end
 
   test "2 different books" do
@@ -61,13 +63,14 @@ defmodule HarryTest do
     assert Harry.price({1, 1, 1, 1, 1}) == (5*8)*0.80
   end
 
-  # test "all together now" do
-  #   assert Harry.price({5, 4, 3, 2, 1}) == (
-  #     (5 * 8 * 80) 
-  #     + (4 * 8 * 85) 
-  #     + (3 * 8 * 90) 
-  #     + (2 * 8 * 95) 
-  #     + 800) /100
-  # end
+  test "all together now" do
+    assert Harry.price({5, 4, 3, 2, 1}) == [
+      (5 * 8 * 0.80),
+      (4 * 8 * 0.85),
+      (3 * 8 * 0.90),
+      (2 * 8 * 0.95),
+      8]
+      |> Enum.sum
+  end
 
 end
